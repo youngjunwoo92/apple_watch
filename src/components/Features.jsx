113e6c3108773eda914f3dbdcab1a2181fafb3b5
youngjun114 <35579719+youngjun114@users.noreply.github.com>
2023-12-07 05:22:01 -0800

    fix: changed div position

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -4,14 +4,13 @@ function Features({ isVisible }) {
   return (
     <div className={`flex h-full flex-col ${isVisible ? 'fade-in' : 'fade-out'}`}>
       <div
-        className={`flex translate-y-[3rem] flex-col items-center p-4 text-center sm:translate-y-[5rem]`}>
+        className={`flex translate-y-[5rem] flex-col items-center p-4 text-center sm:translate-y-[10rem]`}>
         <h2 className="mt-4 text-center text-3xl font-bold font-bold md:text-4xl">
           Advanced Features
         </h2>
         <p className="mt-2 text-xl font-bold text-slate-200 md:text-2xl">
           Empowering Your Lifestyle with Cutting-Edge Technology.
         </p>
-        <p className="text-md mt-4 md:text-xl">A safety feature we hope you’ll never need.</p>
       </div>
     </div>
   );
